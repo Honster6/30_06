883edd703015d8c0c90a76cda670c7d15eab5634
Sebastian <40136322+Honster6@users.noreply.github.com>
2018-06-30 11:19:48 +0200

    zmiana z Integer na T + comment

--- src/stack/StackTest.java
+++ src/stack/StackTest.java
@@ -2,7 +2,7 @@ package stack;
 
 public class StackTest {
     public static void main(String[] args) {
-        /*Stack stack = new Stack (10);
+        Stack stack = new Stack <>(10); // w <np. Integer - tylko liczby> podajemy jaki typ ma przyjac, jesli nie podamy nic to przyjmie wszystko
         System.out.println("Is empty: " + stack.isEmpty()); // sprawdzamy czy stos jest pusty
         stack.push(9); // dolozenie elementu do stosu
         System.out.println("Is empty: " + stack.isEmpty()); //po dolozeniu elementu stos nie jest pusty
@@ -15,9 +15,9 @@ public class StackTest {
         stack.push(234);
         stack.push(45);
         System.out.println("top: " + stack.peek());
-        stack.push(78);
+        stack.push("lalala"); // gdyby przy deklaracji tablicy byloby <Integer> bylby blad, bo moze przyjac tylko liczby
         System.out.println("top: " + stack.peek());
-        */
+
 
         Box<String, Integer> box = new Box ("Jakiś tekst", 40); // po dodaniu <> po new Box nie przyjmie innego typu
         Box<Integer, String> box2 = new Box (172, "tekst "); // mozemy przechowywac rozne typy danych w jednej klasie
